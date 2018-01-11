936c228fbca289e99ff23cdb1948a5fca066c032
José Luis Castro <jluis.scooter11@hotmail.com>
2018-01-10 20:36:45 -0800

    testing firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+//firebase
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  articlesCollection: AngularFirestoreCollection<any>;
+  articles: Observable<any[]>;
+  title = "APP";
+
+  constructor(db: AngularFirestore){
+    this.articlesCollection = db.collection('articles', ref=>{
+      return ref.orderBy('title', 'desc');
+    });
+    this.articles = this.articlesCollection.valueChanges();
+  }
 }
